1620e3724ff55da17cffc4acd0ef6b641168b247
M1ch1dmaa <143465788+M1ch1dmaa@users.noreply.github.com>
2024-12-14 01:46:34 +0800

    Update unittest.py

--- Lab2/unittest.py
+++ Lab2/unittest.py
@@ -1,9 +1,1 @@
-with open("Algorithm\\LAB\\testdivide.txt", "r") as f:
-    line = f.readline()
-    a, b = line.split('    ')
-    print(type(a))
-    print(a)
-    print(type(b))
-    print(b)
-    a = eval(a)
-    b = eval(b)
+.
