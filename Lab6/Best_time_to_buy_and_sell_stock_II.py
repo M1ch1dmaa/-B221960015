0f5a53f00164aa9158f17c767cc6b412cdd70848
M1ch1dmaa <143465788+M1ch1dmaa@users.noreply.github.com>
2024-12-14 02:10:08 +0800

    Create Best_time_to_buy_and_sell_stock_II.py

---
+++ Lab6/Best_time_to_buy_and_sell_stock_II.py
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n= prices.size();
+        int profit = 0;
+        // take into profit only those days for which the next day's price is greater.
+        for(int day=1;day<n;day++){
+            if(prices[day]>prices[day-1]){
+                profit +=(prices[day] - prices[day-1]);
+            }
+        }
+        return profit;
+    }
+};
